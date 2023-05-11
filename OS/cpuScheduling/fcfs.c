deaf14d0d7d468ec7382ef7763bef1ca422d21e2
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-05-11 17:09:37 +0530

    Update fcfs.c

--- OS/cpuScheduling/fcfs.c
+++ OS/cpuScheduling/fcfs.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 void main()
 {
-    int bt[100],at[100],pro[100],ct[100],tat[100],wt[100],n,i,j,temp,temp1,temp2,ttat=0,twt=0;
+    int bt[100],at[100],pro[100],ct[100],tat[100],wt[100],n,i,j,temp,temp1,temp2,ttat=0,twt=0,tbt=0;
     float attat,awt;
     printf("enter number of processes ");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        printf("enter arrival time and bust time of process %d ",i+1);
+        printf("enter arrival time and bust time of process%d ",i+1);
         scanf("%d %d",&at[i],&bt[i]);
         pro[i]=i+1;
+        tbt+=bt[i];
     }
     for(i=0;i<n-1;i++)
     {
@@ -54,7 +55,20 @@ void main()
     for(i=0;i<n;i++)
     printf("\n\t%d\t%d\t%d\t%d\t%d\t%d\n",pro[i],at[i],bt[i],ct[i],tat[i],wt[i]);
     printf("\nAvg TAT=%f\nAvg WT=%f",attat,awt);
-    printf("\nGant Chart \n0");
-    for(i=0;i<n;i++)
-    printf("|P%d| %d|",pro[i],ct[i]);
+    printf("\nGant Chart \n");
+    for(i=0,j=0;i<ct[n-1];)
+    {
+        if(i<at[j])
+        {
+            printf("%d|---",i);
+            i++;
+        }
+        else if(i>=at[j])
+        {
+            printf("|%d|P%d",i,pro[j]);
+            i=ct[j];
+            j++;
+        }
+    }
+    printf("|%d",ct[n-1]);
 }
