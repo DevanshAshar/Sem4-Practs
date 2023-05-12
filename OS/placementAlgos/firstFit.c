5638e2aeb7c22507c46fbe97c68508883d96f4a2
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-05-12 11:01:17 +0530

    Update firstFit.c

--- OS/placementAlgos/firstFit.c
+++ OS/placementAlgos/firstFit.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
-
 void main()
 {
-    int n;
-    int n_mem;
-    int mem_loc[100];
-    printf("Enter no of memory partitions\n");
-    scanf("%d",&n_mem);
-    int mem_sizes[] = {200, 400, 600, 500, 300, 250};
-    int process_sizes[] = {357, 210, 468, 491};
-    printf("Enter no of processes\n");
-    scanf("%d",&n);
-    int j=0;
+    int n_proc=4,n_mem=5,mem_loc[100],visited[100];
+    int mem_sizes[] = {100, 500, 200, 300, 600};
+    int process_sizes[] = {212, 417, 112, 426};
     for(int i=0;i<n_mem;i++)
     {
-        if(process_sizes[j] <= mem_sizes[i])
-        {
-            printf("\nfor %d , %d  %d",j,process_sizes[j],mem_sizes[i]);
-            mem_loc[j] = j+1;
-            j++;
-        }
+        visited[i] = 0;
+        mem_loc[i] = -1;
     }
-    printf("\nFinal\n");
-    for(int i =0;i<n;i++)
+    for(int i=0;i<n_proc;i++)
     {
-        if(mem_loc[i])
+        for(int j=0;j<n_mem;j++)
         {
-            printf("Process size = %d goes in location %d\n",process_sizes[i],mem_loc[i]);
+            if(process_sizes[i] <= mem_sizes[j] && visited[j]==0)
+            {
+                mem_loc[i] = j;
+                visited[j] = 1;
+                break;
+            }
         }
-        else{printf("%d not allocated to memory",process_sizes[i]);}
     }
-
+    printf("\n\n");
+    for(int i =0;i<n_proc;i++)
+    {
+        if(mem_loc[i]!=-1)
+        printf("Process size = %d goes in location %d and hole is %d \n",process_sizes[i],mem_sizes[mem_loc[i]],mem_sizes[mem_loc[i]]-process_sizes[i]);
+        else
+        printf("%d not allocated to memory\n",process_sizes[i]);
+    }
 }
