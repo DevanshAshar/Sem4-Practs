377dd320ecae9c0180ffb9dff99b2736c0a0be0c
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-05-13 11:50:35 +0530

    beech mein blank aata hai , toh error tha, error solved :)

--- OS/cpuScheduling/fcfs.c
+++ OS/cpuScheduling/fcfs.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-    int bt[100],at[100],pro[100],ct[100],tat[100],wt[100],n,i,j,temp,temp1,temp2,ttat=0,twt=0,tbt=0;
+    int bt[100],at[100],pro[100],ct[100],tat[100],wt[100],n,i,j,temp,temp1,temp2,ttat=0,twt=0,tbt=0,gant[100],cgant[100],ind=0;
     float attat,awt;
     printf("enter number of processes ");
     scanf("%d",&n);
@@ -28,14 +28,34 @@ void main()
                 pro[j]=pro[j+1];
                 pro[j+1]=temp2;
             }
+            /*else if(at[j]==at[j+1])
+            {
+                if(bt[j]>bt[j+1])
+                {
+                    temp=at[j];
+                    at[j]=at[j+1];
+                    at[j+1]=temp;
+                    temp1=bt[j];
+                    bt[j]=bt[j+1];
+                    bt[j+1]=temp1;
+                    temp2=pro[j];
+                    pro[j]=pro[j+1];
+                    pro[j+1]=temp2;
+                }
+            }*/
         }
     }
     int prevBt=at[0];
     //ct
     for(i=0;i<n;i++)
     {
+        gant[ind]=pro[i];
         ct[i]=prevBt+bt[i];
+        cgant[ind]=ct[i];
+        ind++;
         prevBt=ct[i];
+        if(prevBt<at[i+1])
+        prevBt=at[i+1];
     }
     //tat wt
     for(i=0;i<n;i++)
@@ -56,19 +76,10 @@ void main()
     printf("\n\t%d\t%d\t%d\t%d\t%d\t%d\n",pro[i],at[i],bt[i],ct[i],tat[i],wt[i]);
     printf("\nAvg TAT=%f\nAvg WT=%f",attat,awt);
     printf("\nGant Chart \n");
-    for(i=0,j=0;i<ct[n-1];)
-    {
-        if(i<at[j])
-        {
-            printf("%d|---",i);
-            i++;
-        }
-        else if(i>=at[j])
-        {
-            printf("|%d|P%d",i,pro[j]);
-            i=ct[j];
-            j++;
-        }
-    }
-    printf("|%d",ct[n-1]);
+    printf("\nGant Chart \n");
+    for(i=0;i<ind;i++)
+    printf("P%d\t",gant[i]);
+    printf("\n");
+    for(i=0;i<ind;i++)
+    printf("%d\t",cgant[i]);
 }
