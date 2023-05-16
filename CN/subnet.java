99cd62cdc2e9d0c0c8977ef3562b0fdc2597e99d
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-05-16 16:21:27 +0530

    Create subnet.java

---
+++ CN/subnet.java
@@ -0,0 +1,79 @@
+import java.util.*;
+class subnet
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String ip,first,mask=" ",subnetAdd="";
+        char clas=' ',ch;
+	int i,k=0,ct=0;
+        int ind[]=new int[3];
+        System.out.println("Enter ip address ");
+        ip=sc.nextLine();
+        for(i=0;i<ip.length();i++)
+        {
+            ch=ip.charAt(i);
+            if(ch=='.')
+            {
+                ind[k++]=i;
+                ct++;
+            }
+        }
+        if(ct==3)
+        {
+            first=ip.substring(0,ind[0]);
+            int f=Integer.parseInt(first);
+            if(f>=0 && f<=127)
+            {
+                clas='A';
+                mask="255.0.0.0";
+            }
+            else if(f>=128 && f<=191)
+            {
+                clas='B';
+                mask="255.255.0.0";
+            }
+            else if(f>=192 && f<=223)
+            {
+                clas='C';
+                mask="255.255.255.0";
+            }
+            else if(f>=224 && f<=239)
+            clas='D';
+            else if(f>=240 && f<=255)
+            clas='E';
+            else
+            System.out.println("Invalid IP Address");
+            if(clas=='A' || clas=='B' || clas=='C')
+            {
+                System.out.println("Subnet class "+clas);
+                System.out.println("Subnet Mask "+mask);
+            }
+            else if(clas=='D')
+            {
+                System.out.println("Subnet class "+clas);
+                System.out.println("Reserved for multicast");
+            }
+            else if(clas=='E')
+            {
+                System.out.println("Subnet class "+clas);
+                System.out.println("Experimental");
+            }
+            String ipArr[]=ip.split("\\.");
+            if(mask!=" ")
+            {
+                String maskArr[]=mask.split("\\.");
+                for(i=0;i<4;i++)
+                {
+                    int x=Integer.parseInt(ipArr[i]);  
+                    int y=Integer.parseInt(maskArr[i]); 
+                    int z=x&y;
+                    subnetAdd+=z+".";
+                }
+                System.out.println("Subnet address is "+subnetAdd);
+            }
+        }
+        else
+        System.out.println("Invalid IP Address");
+    }
+}
