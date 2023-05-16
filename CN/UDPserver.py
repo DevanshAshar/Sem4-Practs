812e3497320e7d92ef0d58b79d105ad0dbffa4da
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-05-16 22:03:54 +0530

    Create UDPserver.py

---
+++ CN/UDPserver.py
@@ -0,0 +1,14 @@
+import socket
+host="127.0.0.1"
+port=1234
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((host,port))
+while True:
+	print("server on...")
+	data,addr = sock.recvfrom(1024)
+	if data.decode("utf-8") == "close":
+		sock.close
+		break;
+	else:
+		print("from client "+data.decode("utf-8"))
+	
