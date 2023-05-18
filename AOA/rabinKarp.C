1fdc15e9ab56e89f6e832d5fec39334c043cc4d2
DevanshAshar <113104126+DevanshAshar@users.noreply.github.com>
2023-05-18 17:04:50 +0530

    Acc to algo :(

--- AOA/rabinKarp.C
+++ AOA/rabinKarp.C
@@ -3,19 +3,25 @@
 #include<math.h>
 void main()
 {
-    char txt[] = "xyzahxyfxyzd";
-    char pat[] = "xyz";
-    int pathash=0,i,j,k,txthash,flag=0;
+    char txt[] = "abcbcdabc";
+    char pat[] = "abc";
+    int pathash=0,i,j,k,txthash=0,flag=0;
     for(i=0;i<strlen(pat);i++)
-    pathash+=pat[i]*pow(10,strlen(pat)-i-1);
-    for(i=0;i<strlen(txt);i++)
     {
-        txthash=0;
-        for(j=i,k=0;j<strlen(pat)+i;j++,k++)
-        txthash+=txt[j]*pow(10,strlen(pat)-k-1);
+        pathash+=pat[i]*pow(10,strlen(pat)-i-1);
+        txthash+=txt[i]*pow(10,strlen(pat)-i-1);
+    }
+    if(txthash==pathash)
+    {
+        printf("String Matched at index 0\n");
+        flag=1;
+    }
+    for(i=strlen(pat);i<strlen(txt);i++)
+    {
+        txthash=(txthash-((txt[i-strlen(pat)])*pow(10,strlen(pat)-1)))*10+txt[i];
         if(txthash==pathash)
         {
-            printf("String Matched at index %d\n",i);
+            printf("String Matched at index %d\n",i-strlen(pat)+1);
             flag=1;
         }
     }
